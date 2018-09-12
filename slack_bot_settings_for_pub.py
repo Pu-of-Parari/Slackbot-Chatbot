f20ef941d26e5d7a8a2ae6fdcc97debe66a58bb9
Pu-of-Parari <40864878+Pu-of-Parari@users.noreply.github.com>
2018-09-12 15:57:31 +0900

    Add files via upload

---
+++ slack_bot_settings_for_pub.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# botアカウントのトークンを指定
+API_TOKEN = "xxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "What r u saying?"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
