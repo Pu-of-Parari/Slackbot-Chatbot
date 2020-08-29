952b42197138baf058d439cde83a43455fcfe45a
KONTA2019 <58900062+KONTA2019@users.noreply.github.com>
2020-08-29 19:45:04 +0900

    自分のtoken追加

--- slack_bot_settings.py
+++ slack_bot_settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # botアカウントのトークンを指定
-API_TOKEN = "xxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx"
+API_TOKEN = "xoxb-1202686154566-1331160701858-jTZKbmG0X9iyc6nQaExakSm7"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "What r u saying?"
